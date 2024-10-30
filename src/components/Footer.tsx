6a737dfd01c13223e144929ea4ae09df44e826b1
mubiana65 <mubiana65@gmail.com>
2024-10-30 05:46:26 +0200

    first commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-blue-900 dark:bg-darkBackground text-white dark:text-gray-200 py-5">
       <div className="container mx-auto text-center">
-        <p>&copy; 2024 ITRPID. All rights reserved.</p>
+        <p>&copy; 2024 ITREPID. All rights reserved.</p>
         <p className="mt-2">
           <a href="#contact" className="hover:text-gray-300">Contact Us</a> | Follow us on 
           <a href="#" className="hover:text-gray-300 ml-2">LinkedIn</a>
